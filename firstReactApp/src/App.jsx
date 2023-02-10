eae63807218bc04c63ee8bd54999a5b1afb0cd57
sagar chawla <sagarchawla83@gmail.com>
2023-02-10 13:04:07 +0530

    testing vite deployment

---
+++ firstReactApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState , useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ firstReactApp/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar(){
+    return (
+        <nav >
+            <a>
+                Earthly
+            </a>
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
